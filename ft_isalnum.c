2e0c2ae4c46be0cfba4e702f5f6d9ed0e1247e8d
martisvian <martis.vian@yahoo.com>
2015-10-24 14:38:32 +0000

    ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,6 +1,10 @@
 int ft_isalnum(char c)
 {
         if(c >= 48 && c <= 57)          
+                return (1);
+        if(c >= 65 && c <= 90)          
                 return (1);     
-        return (0);
+        if(c >= 97 && c <= 122) 
+                return (1);
+	return (0);
 }
