c9c817c82368fad4439455c6dd7dc2062a2a46f4
vadochiei <vlad_rock_boy_bad@yahoo.com>
2015-10-24 16:16:03 +0300

    add ft_strcmp.c

---
+++ ft_strcmp.c
@@ -0,0 +1,15 @@
+int ft_strcmp(const char *s1, const char *s2)
+{
+	int q;
+	int i;
+
+	i = 0;
+	q = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
