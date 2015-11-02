710f07626a44468a4766ef57c78def4a2805b0d6
martisvian <martis.vian@yahoo.com>
2015-11-02 17:05:40 +0000

    ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,18 @@
+void	*ft_memcpy(void *s1, const void *s2, int n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	int		i;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	if (str1 == '\0' || str2 == '\0')
+		return (0);
+	while (i < n)
+	{
+		str1[i] = str2[i];
+		i++;
+	}
+	return (str1);
+}
